cf98e7837faa2a611fc40dce763be7445d97f900
Xavi Vicient <xavi@iberphoto.com>
2012-05-11 19:39:31 +0200

    Creada rutina bucle() que realiza los disparos cuando toca

--- trabajov1/trabajo.h
+++ trabajov1/trabajo.h
@@ -27,6 +27,9 @@ char tocacam1=0, tocacam2=0;
 unsigned int dspCam1=0, dspCam2=0;		//Disparos de la camara
 unsigned long tpCam1=0, tpCam2=0;		//Tiempo entre disparos
 
+unsigned char fintrabajo=0;				//Variable que controla si ha finalizado el trabajo
+unsigned long contador_trabajo1=0,contador_trabajo2=0;		//Contadores de segundos
+unsigned int contadorsdspCam1=0, contadorsdspCam2=0;
 
 //Prototipos de la libreria del PSoC de trabajo
 void inicializacion(void);
@@ -36,7 +39,10 @@ void disparo_sensores(void);
 void ejecucion(void);
 void preparadisparo(void);
 unsigned long calculosegundos(char numero, char unidades);
+void bucle(void);
 void disparo(void);
+void envia_fintrabajo(void);
+
 
 //******************************************************************************
 //******************************************************************************
@@ -166,7 +172,7 @@ void disparo_sensores(void)
 	
 	//Disparar segun tipo de disparo
 	if (sensores!=0) 
-	disparo();
+	bucle();
 }
 //******************************************************************************
 //******************************************************************************
@@ -184,7 +190,12 @@ void disparo_sensores(void)
 void ejecucion (void)
 {
 	preparadisparo();
-	disparo();
+	do
+	{
+		bucle();
+	}
+	while(fintrabajo!=255);
+	envia_fintrabajo();
 }	
 //******************************************************************************
 //******************************************************************************
@@ -205,22 +216,20 @@ void preparadisparo(void)
 	#define tclip preparadisparo_tp_clip
 	
 	unsigned long treal, tclip;
-	
-	switch (Cam1==)				//Cámara 1
+	tocacam1=on;
+	tocacam2=on;
+	switch (Cam1)				//Cámara 1
 	{
-		case 1:					//disparo único
-			tocacam1=on;
-			break
 		case 2:					//intervalometro
 			dspCam1=Int1_Ndisp;
 			tpCam1=calculosegundos(Int1_Tdisp, Int1_Tdisp_Uni);
-			break
+			break;
 		case 3:					//Timelapse
 			treal=calculosegundos(TL1_Treal, TL1_Treal_Uni);	//Calculo en segundos del tiempo real
 			tclip=calculosegundos(TL1_Tfilm, TL1_Tfilm_Uni);	//Calculo en segundos del tiempo de clip
 			dspCam1=(tclip*25);		//25 fotogramas por segundo para Europa (PAL)
 			tpCam1=(treal/dspCam1);
-			break
+			break;
 	}
 
 	//Falta un switch igual para la camara 2 despues que Javi confirme la redacción
@@ -252,6 +261,70 @@ unsigned long calculosegundos(char numero, char unidades)
 
 
 /************************************************************************************************************************
+/  	LLAMADA: bucle()
+/  	FUNCION: Rutina que decide cuando se deben disparar las cámaras/flashes
+/  	ENTRADA: void
+/  	SALIDA: void
+/	OTROS: nada
+/  	AUTOR: Rutina realizada por Albert Sagol y Xavi Vicient para el proyecto de C4 y C9
+/**********************************************************************************************************************/
+void bucle(void)
+{	
+	char bucle_acaba1=0, bucle_acaba2=0;
+	char fintrabajo1=0, fintrabajo2=0;
+	
+	do
+	{
+		disparo();
+		
+		//Start de los contadores de trabajo
+		
+		if (bucle_acaba1==off)
+		{
+			if (Cam1==1) bucle_acaba1=on;
+			else
+			{
+				if (contador_trabajo1==tpCam1) 
+				{
+					tocacam1=on;
+					contador_trabajo1=0;
+				}
+				if (contadorsdspCam1==dspCam1)
+				{
+					bucle_acaba1=on;
+					fintrabajo1=255;
+				}
+			}
+		}
+		
+		if (bucle_acaba2==off)
+		{
+			if (Cam2==1) bucle_acaba2=on;
+			else
+			{
+				if (contador_trabajo2==tpCam2) 
+				{
+					tocacam2=on;
+					contador_trabajo2=0;
+				}
+				if (contadorsdspCam2==dspCam2)
+				{
+					bucle_acaba2=on;
+					fintrabajo2=255;
+				}
+			}
+		}	
+	}
+	while (bucle_acaba1==off || bucle_acaba2==off);
+	
+	if (fintrabajo1==255 && fintrabajo2==255) fintrabajo=255;
+}
+//******************************************************************************
+//******************************************************************************
+
+
+
+/************************************************************************************************************************
 /  	LLAMADA: disparo()
 /  	FUNCION: Rutina que dispara la/s camara/s y/o flashes segun programación
 /  	ENTRADA: void
@@ -267,11 +340,15 @@ void disparo(void)
 		{
 			PRT2DR=PRT2DR | 0x01;	//activar enfoque cam1
 			PRT2DR=PRT2DR | 0x04;	//activar obturador cam1
+			tocacam1==off;
+			contadorsdspCam1++;
 		}
 	if (tocacam2==on)
 		{
 			PRT2DR=PRT2DR | 0x10;	//activar enfoque cam2
 			PRT2DR=PRT2DR | 0x40;	//activar obturador cam2
+			tocacam2==off;
+			contadorsdspCam2++;
 		}
 	//Apertura de los flashes
 	if (Fla1==1) PRT0DR=PRT0DR | 0x01;	
@@ -287,7 +364,21 @@ void disparo(void)
 	
 	//Desactivación de las cámaras
 	PRT2DR=PRT2DR & 0xAA;			//1010-1010
-	
+}
+//******************************************************************************
+//******************************************************************************
+
+/************************************************************************************************************************
+/  	LLAMADA: envia_fintrabajo()
+/  	FUNCION: Rutina que envia mediante UART un 255 para indicar al PSoC que ha finalizado el trabajo
+/  	ENTRADA: void
+/  	SALIDA: void
+/	OTROS: Necesaria la UART y el Timer asociado como periféricos
+/  	AUTOR: Rutina realizada por Albert Sagol y Xavi Vicient para el proyecto de C4 y C9
+/**********************************************************************************************************************/
+void bucle(void)
+{
+
 }
 //******************************************************************************
 //******************************************************************************
